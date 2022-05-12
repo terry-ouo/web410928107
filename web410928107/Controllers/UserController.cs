bb0bd568eb0b492d94ccc347edad03ac873eb66d
Stinckygoo <91928322+Stinckygoo@users.noreply.github.com>
2022-05-12 20:36:46 +0800

    2022/05/12 add user Sign up function

---
+++ web410928107/Controllers/UserController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace web410928107.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: User
+        public ActionResult Signup(string name, string account, string password)
+        {
+            if (string.IsNullOrWhiteSpace(name))
+            {
+                ViewBag.NameMessage = "請輸入姓名";
+            }
+            else if (string.IsNullOrWhiteSpace(account))
+            {
+                ViewBag.NameMessage = "請輸入帳號";
+            }
+            else if (string.IsNullOrWhiteSpace(password))
+            {
+                ViewBag.NameMessage = "請輸入密碼";
+            }
+            else
+            {
+                ViewBag.NameMessage = "註冊成功";
+            }
+            return View();
+        }
+    }
+}
